2fcb7682625e2b9ab43a9bec88e096b6325860a0
熊政 <zheng.xiong@tcredit.com>
2018-11-13 09:40:52 +0800

    增加一些注释

--- app/src/main/java/demo/intentdemo/com/myapplication/MainActivity.java
+++ app/src/main/java/demo/intentdemo/com/myapplication/MainActivity.java
@@ -5,9 +5,11 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Environment;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import java.io.File;
@@ -23,6 +25,21 @@ public class MainActivity extends AppCompatActivity {
                 requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},100);
             }
         }
+
+
+        //最详细的那篇博客有讲这些路径的区别在哪里
+        //内部存储空间应用私有目录下的 files/ 目录
+        File filesDir = getFilesDir();
+        //内部存储空间应用私有目录下的 cache/ 目录
+        File cacheDir = getCacheDir();
+        //外部存储空间根目录
+        File file = Environment.getExternalStorageDirectory();
+        // 外部存储空间应用私有目录下的 files/
+        File file1 = getExternalFilesDir(null);
+        //外部存储空间应用私有目录下的 cache/
+        File file2 = getExternalCacheDir();
+        Log.e("---- :",file2+"");
+
     }
 
     public void haha(View view){
